77ffd5436e10e0544ec067b6e6d5c0c8a676c9cf
yoad sassoni <yogevsas@gmail.com>
2025-07-15 15:59:21 +0300

    converted to class

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+class Movie:
+    def __init__(self, name, rating, genre):
+        self.name = name
+        self.rating = rating
+        self.genre = genre
+
+    def __str__(self):
+        return f"Movie Name: {self.name}, Rating: {self.rating}, Genre: {self.genre}"
+
+    def __repr__(self):
+        return f"Movie(name='{self.name}', rate={self.rating}, genre='{self.genre}')"
+
+
 def get_movie_list():
-    movie_dict = {}
+    movies = []
     while True:
         user_input = input("Enter a movie title (or type 'finish' to end): ")
         if user_input.lower() == 'finish':
             break
-        if user_input in movie_dict:
+        if any(movie.name == user_input for movie in movies):
             print("You've already rated this movie.")
             continue
-        movie_dict[user_input] = rate_movie(user_input)
-    return movie_dict
+        movies.append(Movie(user_input, rate_movie(user_input), cluster_movie_to_genre(user_input)))
+    return movies
 
 
 def rate_movie(movie):
@@ -22,18 +35,23 @@ def rate_movie(movie):
             print("Invalid input. Enter a number between 1 and 10.")
 
 
-def get_avg_rating(movie_dict):
-    return sum(movie_dict.values()) / len(movie_dict)
+def cluster_movie_to_genre(movie):
+    genre = input(f"Genre of '{movie}' : ")
+    return genre
 
 
-def get_max_rating_movies(movie_dict):
-    max_rating = max(movie_dict.values())
-    return [movie for movie, rating in movie_dict.items() if rating == max_rating]
+def get_avg_rating(movies):
+    return sum(movie.rating for movie in movies) / len(movies)
 
 
-def get_min_rating_movies(movie_dict):
-    min_rating = min(movie_dict.values())
-    return [movie for movie, rating in movie_dict.items() if rating == min_rating]
+def get_max_rating_movies(movies):
+    max_rating = max(movie.rating for movie in movies)
+    return [movie for movie in movies if movie.rating == max_rating]
+
+
+def get_min_rating_movies(movies):
+    min_rating = min(movie.rating for movie in movies)
+    return [movie for movie in movies if movie.rating == min_rating]
 
 
 def main():
